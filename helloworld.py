7dea7ff8b982392b0b978f66a233a3f1bc9c51dd
Karan Goel <karan@goel.im>
2013-10-26 11:47:19 -0700

    initial commit

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+# Simple program that prints Hello World on screen.
+
+'''
+@author: Karan Goel
+@email: karan@goel.im
+'''
+
+for i in xrange(10):
+    print 'Hello world'
---
+++ sample.py
@@ -0,0 +1,12 @@
+x = 2 # an int
+y = 5 # another int
+z = 3.14 # a float
+
+print x + y
+
+if x + y % 2 == 0:
+    print 'You are doomed'
+else:
+    print 'You are still doomed'
+
+print z
